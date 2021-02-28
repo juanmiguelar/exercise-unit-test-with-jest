d213740e0c12218bbff186e0b6f2731b49d8d86f
Juan Miguel <juan.ariasmejias@ucr.ac.cr>
2021-02-28 02:38:44 +0000

    pruebas

---
+++ app.js
@@ -0,0 +1,25 @@
+const oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+
+const sum = (a, b) => {
+    return a + b;
+}
+
+const fromDollarToYen = (dollar) => {
+    return (dollar / 1.2)*127.9;
+}
+
+const fromEuroToDollar = (euro) => {
+    return euro * 1.2;
+}
+
+const fromYanToPound = (yan) => {
+    return (yan / 127.5) * 0.8;
+}
+
+console.log(fromDollarToYen(1))
+
+module.exports = {sum, fromDollarToYen, fromEuroToDollar, fromYanToPound}
